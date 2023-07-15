f06da39e36411bc16e097aded25d15db03907c67
Lars <32791312+larsl2005@users.noreply.github.com>
2023-07-15 21:57:28 +0200

    Added the ability to install as a "Launcher"
    This makes it easier to run under Linux

--- StandConsoleInjector/Program.cs
+++ StandConsoleInjector/Program.cs
@@ -7,10 +7,13 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -79,30 +82,179 @@ public class Program
     [DllImport("kernel32.dll", SetLastError = true)]
     private static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttribute, IntPtr dwStackSize,
         IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
-    
-    static void Main ()
+
+    static void Main(string[] args)
+	{
+        Console.ForegroundColor = ConsoleColor.Magenta;
+		if(!args.Contains("-NuhUh"))
+			CheckIfRunningInShittyHackMode(args);
+		_Main();
+	}
+
+	//To anyone reading this code i am so sorry for the spaggetti monster you are about to witness
+	static void CheckIfRunningInShittyHackMode(string[] args)
+	{
+        //Steam: PlayGTAV.exe
+        //Epic: Redistributables/Rockstar-Games-Epic.exe
+        string LauncArgs = "";
+		string eeee = "";
+
+        bool IsEpicAndShouldRelaunch = false;
+
+        bool IsRunningHackyMode = false;
+        string ProcNameToStart = "N/A";
+
+        foreach (object arg in args)
+        {
+            if (arg == "-StraightIntoFreemode") //only possible on the steam version iirc
+                Console.WriteLine("BRO WHY THE FUCK WOULD YOU DO THAT!!! XDDDDD");
+            LauncArgs = LauncArgs + arg + " ";
+			Skip:
+				eeee = eeee + arg.GetType().ToString() + " ("+arg+") ";
+        }
+		//File.WriteAllText("log.txt", Process.GetProcessById(Environment.ProcessId).MainWindowTitle + "\n" + LauncArgs + "\n" + eeee);
+
+        if (Environment.ProcessPath.ToLower().Contains("playgtav")) //unused code from when i was fucking around trying to get the epic verison to work, too lazy to remove it
+            ProcNameToStart = "_PlayGTAV.exe";
+		if (File.Exists("Redistributables\\Rockstar-Games-Epic.exe"))
+			IsEpicAndShouldRelaunch = true;
+
+		if (ProcNameToStart != "N/A")
+			IsRunningHackyMode = true;
+
+        //Console.WriteLine("path: " + Environment.CurrentDirectory + "\nprocname: " + ProcNameToStart);
+
+
+        if (IsRunningHackyMode)
+		{
+            if (File.Exists(ProcNameToStart))
+                goto Whatever;
+
+            Console.WriteLine("Could not find renamed "+ProcNameToStart.Substring(1)+", enter the new name manually");
+            ProcNameToStart = Console.ReadLine();
+            if (!ProcNameToStart.Contains(".exe"))
+                ProcNameToStart = ProcNameToStart + ".exe";
+
+			Whatever:
+			Process.Start(ProcNameToStart, args);
+
+			if (IsEpicAndShouldRelaunch)
+				Process.Start(Environment.ProcessPath, "-NuhUh " + LauncArgs);
+        }
+	}
+
+	static void InstallLauncherForShittyHackMode()
+	{
+		if (!Environment.ProcessPath.ToLower().Contains("playgtav"))
+		{
+			Console.Clear();
+			string ToPrint = "READ THIS ENTIRELY BEFORE INSTALLING:\nI have no idea if injecting this way is detected, I haven't tested it long enough to know (I don't THINK it is though)\n" +
+				"USE AT YOUR OWN RISK!!!!\nThis will make the injector launch through your desired launcher, then the injector will launch the game, this makes it easier to run under linux\n"
+				+ "THIS WILL MOST LIKELY ONLY WORK WITH THE DOTNET NATIVE VERSION OF THE INJECTOR, MAKE SURE YOU ARE USING THAT VERSION!\n\n\nPress any key to continue..";
+
+			Console.WriteLine(ToPrint);
+			Console.ReadKey();
+			Console.Clear();
+		Insert:
+			Console.WriteLine("Insert your gta install directory");
+			string dir = Console.ReadLine();
+			if (File.Exists(dir + "\\PlayGTAV.exe"))
+			{
+				if (File.Exists(dir + "\\_PlayGTAV.exe"))
+				{
+					Console.WriteLine("Launcher most likely already installed!");
+					Console.WriteLine("Do you want to:\n[1] Update\n[2] Uninstall\nIf your game just updated. and the launcher does NOT work, press update");
+				UpdateKeyInput:
+					ConsoleKey key = Console.ReadKey().Key;
+					if (key == ConsoleKey.D1)
+						goto Skip;
+					else if (key == ConsoleKey.D2)
+						Uninstall(dir);
+					else
+						goto UpdateKeyInput;
+
+				}
+
+				Console.WriteLine("Found gta launcher!");
+				File.Copy(dir + "\\PlayGTAV.exe", dir + "\\_PlayGTAV.exe");
+
+			Skip:
+				File.Delete(dir + "\\PlayGTAV.exe");
+				File.Copy(Environment.ProcessPath, dir + "\\PlayGTAV.exe");
+				Console.WriteLine("Succsess!");
+				_Main();
+			}
+			else
+			{
+				Console.WriteLine("Gta V Not found!");
+				goto Insert;
+			}
+		}
+		else
+		{
+			Console.WriteLine("Cannot install launcher as launcher!");
+		}
+    }
+
+	static void Uninstall(string dir)
+	{
+        if (Environment.ProcessPath.Contains("PlayGTAV"))
+        {
+            Console.WriteLine("Cannot uninstall launcher from launcher mode\nRun the injector outside the GTA folder AND with a different name that \"PlayGTAV.exe\"");
+            _Main();
+        }
+		else
+		{
+			if (!IsAdministrator())
+				Console.WriteLine("Not running as admin, uninstall/cleanup might fail!");
+			File.Delete(dir + "\\PlayGTAV.exe");
+			File.Copy(dir + "\\_PlayGTAV.exe", dir + "\\PlayGTAV.exe");
+			try
+			{
+				File.Delete(dir + "\\_PlayGTAV.exe");
+			}
+			catch
+			{
+				Console.WriteLine("Failed to cleanup, but original launcher has been restored");
+			}
+			Console.WriteLine("Uninstalled");
+			_Main();
+		}
+    }
+
+	static void _Main ()
     {
         Console.ForegroundColor = ConsoleColor.Magenta;
-        Console. WriteLine("This is based on The Stand Launchpad originally made by Calamity Inc (https://calamity.gg).\nThis is very rough, and was never really meant to be public.\nif you run into issues create an issue on github: https://github.com/larsl2005/StandConsoleInjector\nI am NOT associated with Stand and/or Calamity Inc.\nPasted by 0harmony\nOPTIONS\n[1] Check for Stand updates\n[2] Inject\n[3] Exit");
+        Console. WriteLine("This is based on The Stand Launchpad originally made by Calamity Inc (https://calamity.gg).\nThis is very rough, and was never really meant to be public." +
+			"\nif you run into issues create an issue on github: https://github.com/larsl2005/StandConsoleInjector\nI am NOT associated with Stand and/or Calamity Inc." +
+			"\nPasted by 0harmony\nOPTIONS\n[1] Check for Stand updates\n[2] Inject\n[3] Install/Uninstall launcher\n[4] Exit");
 		ConsoleKey key = Console.ReadKey().Key;
-        if (key == ConsoleKey.D1)
-            UpdateStart();
-		if (key == ConsoleKey.D2)
-			inject();
-		else
-			Main();
+		switch (key)
+		{
+			case ConsoleKey.D1:
+                UpdateStart();
+				return;
+			case ConsoleKey.D2:
+                inject();
+				return;
+			case ConsoleKey.D3:
+				InstallLauncherForShittyHackMode();
+				return;
+			case ConsoleKey.D4:
+				Environment.Exit(0);
+				return;
+			default:
+				_Main();
+				return;
+		}
     }
 
     static void EnsureStandFolderExists()
     {
         if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Stand"))
-        {
-            Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Stand");
-        }
+			Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Stand");
         if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Stand\\Bin"))
-        {
-            Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Stand\\Bin");
-        }
+			Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Stand\\Bin");
     }
 
     static void UpdateStart()
@@ -113,7 +265,7 @@ public class Program
 		Console.WriteLine("Latest version: " + GetLatestStandVersion());
 		Thread.Sleep(2000);
         downloadStandDll();
-		Main();
+		_Main();
     }
     
 	static string GetLatestStandVersion()
@@ -148,7 +300,6 @@ public class Program
     private static bool downloadStandDll()
     {
 	    stand_dll = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Stand\\Bin\\Stand " + Stand_DLL_Version + ".dll";
-	    //IL_00a9: Unknown result type (might be due to invalid IL or missing references)
 	    bool result = true;
 	    Console.WriteLine("Downloading Stand");
 	    download_progress = 0;
@@ -184,9 +335,6 @@ public class Program
 	{
 
 		updateGtaPid();
-		//IL_02b7: Unknown result type (might be due to invalid IL or missing references)
-		//IL_032e: Unknown result type (might be due to invalid IL or missing references)
-		
 		bool flag = false;
 		List<string> list = new List<string>();
 		list.Add(stand_dll);
@@ -198,20 +346,16 @@ public class Program
 		}
 		else
 		{
-			Console.WriteLine("Doing stuff 1");
+			Console.WriteLine("Getting Femboy Ready For Sacrifice");
 			IntPtr procAddress = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryW");
 			if (procAddress == IntPtr.Zero)
-			{
-				Console.WriteLine("Failed to find LoadLibraryW.");
-			}
+				Console.WriteLine("Femboy Sacrifice failed (Failed to find LoadLibraryW.)");
 			else
 			{
-                Console.WriteLine("Doing stuff 2");
+                Console.WriteLine("Sacrificing Femboy");
                 string text = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Stand\\Bin\\Temp";
 				if (!Directory.Exists(text))
-				{
 					Directory.CreateDirectory(text);
-				}
 				try
 				{
 					foreach (string item in list)
@@ -226,17 +370,11 @@ public class Program
 						byte[] bytes = Encoding.Unicode.GetBytes(text2);
 						IntPtr intPtr2 = VirtualAllocEx(intPtr, (IntPtr)(void*)null, (IntPtr)bytes.Length, 12288u, 64u);
 						if (intPtr2 == IntPtr.Zero)
-						{
 							Console.WriteLine("Couldn't allocate the bytes to represent " + item);
-						}
 						else if (WriteProcessMemory(intPtr, intPtr2, bytes, (uint)bytes.Length, 0) == 0)
-						{
 							Console.WriteLine("Couldn't write " + text2 + " to allocated memory");
-						}
 						else if (CreateRemoteThread(intPtr, (IntPtr)(void*)null, IntPtr.Zero, procAddress, intPtr2, 0u, (IntPtr)(void*)null) == IntPtr.Zero)
-						{
 							Console.WriteLine("Failed to create remote thread for " + item);
-						}
 						else
 						{
 							num++;
@@ -251,16 +389,18 @@ public class Program
 			}
 			CloseHandle(intPtr);
 		}
-		Console.WriteLine("Probably Injected");
+		
 		if (num == 0)
 		{
 			if (!any_successful_injection && list.Count != 0 && !flag)
-			{
-				Console.WriteLine("Nothing was injected.");
-			}
+				Console.WriteLine("Sacrifice Failed (Nothing was injected.)");
 			//EnableReInject();
 		}
-		Main();
+		else
+		{
+            Console.WriteLine("Femboy Sacrificed Succesfully (Injected)");
+        }
+		_Main();
 
 	}
 
@@ -274,22 +414,27 @@ public class Program
             if (process.ProcessName == "GTA5")
             {
                 if (gta_pid == process.Id)
-                {
                     return;
-                }
                 gta_pid = process.Id;
                 game_was_open = true;
 				Console.WriteLine("Gta found: PID " + gta_pid);
                 return;
             }
             else
-            {
                 i++;
-            }
         }
         Console.WriteLine("Gta not found");
         bool result = gta_pid != 0;
         gta_pid = 0;
+    }
+
+
+	//stack overflow my beloved
+	private static bool IsAdministrator()
+    {
+        WindowsIdentity identity = WindowsIdentity.GetCurrent();
+        WindowsPrincipal principal = new WindowsPrincipal(identity);
+        return principal.IsInRole(WindowsBuiltInRole.Administrator);
     }
 
 }
